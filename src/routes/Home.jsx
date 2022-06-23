4d19f6af0708fe7a26d58dcc15c253c00396cdf3
John Swan <johneswan3@protonmail.com>
2022-06-23 11:53:06 -0600

    Trending section

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CoinSearch from "../components/CoinSearch";
+import Trending from "../components/Trending";
 
 function Home({ coins }) {
   return (
     <div>
       <h1>
         <CoinSearch coins={coins} />
+        <Trending />
       </h1>
     </div>
   );

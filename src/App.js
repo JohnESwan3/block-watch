788ae8a7afadcbc225c25770740443d0c4e564ae
John Swan <johneswan3@protonmail.com>
2022-06-23 11:55:44 -0600

    Added key to trending, setting up routing for coins

--- src/App.js
+++ src/App.js
@@ -29,6 +29,9 @@ function App() {
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/account" element={<Account />} />
+        <Route path="/coin/:coinId" element={<CoinPage />}>
+          <Route path=":coinId" />
+        </Route>
       </Routes>
     </ThemeProvider>
   );
